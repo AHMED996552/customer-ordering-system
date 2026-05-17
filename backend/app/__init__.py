32f5f9f7edffff94ab9655b0ca90d4848c64a395
Hussain <hussain@luxeeats.dev>
2026-05-17 20:20:23 +0300

    add cors support to uc-1 backend

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask_cors import CORS
 
 
 def create_app(testing: bool = False) -> Flask:
@@ -6,6 +7,8 @@ def create_app(testing: bool = False) -> Flask:
 
     if testing:
         app.config["TESTING"] = True
+
+    CORS(app, origins=["http://localhost:3000", "http://localhost:3001"])
 
     from app.routes.restaurants import bp as restaurants_bp
     app.register_blueprint(restaurants_bp)
